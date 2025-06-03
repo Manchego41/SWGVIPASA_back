801bf536247aeb07780b92ee751e856c3114927e
Manchego41 <202110494@urp.edu.pe>
2025-06-02 23:22:15 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 
 connectDB();
-app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
+app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`)); 
